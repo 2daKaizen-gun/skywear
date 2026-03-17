e3d17d8c3d17dea06ff590321f8392e2f679086b
2daKaizen-gun <hkys1223@gmail.com>
2026-03-17 15:22:13 +0900

    style: configure typography for temperature and city display 2

--- SkyWear/app/src/main/java/com/kaizen/skywear/ui/theme/Type.kt
+++ SkyWear/app/src/main/java/com/kaizen/skywear/ui/theme/Type.kt
@@ -8,7 +8,23 @@ import androidx.compose.ui.unit.sp
 
 val Typography = Typography(
     // 1. 온도 표시(크고 강조됨)
+    displayLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 64.sp,
+        lineHeight = 72.sp,
+        letterSpacing = (-0.5).sp
+    ),
     // 2. 도시 이름(굵고 선명함)
+    headlineMedium = TextStyle(
+
+    ),
     // 3. 메인 코디 추천
+    bodyLarge = TextStyle(
+
+    ),
     // 4. 부가 정보 (습도나 바람 등 작은 텍스트)
+    labelMedium = TextStyle(
+
+    )
 )
