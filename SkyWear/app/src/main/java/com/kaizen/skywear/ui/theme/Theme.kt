266c67b7eecb3a957ad29c8df3241616b0fccfa1
2daKaizen-gun <hkys1223@gmail.com>
2026-03-17 15:09:01 +0900

    style: integrate custom color schemes into MaterialTheme

--- SkyWear/app/src/main/java/com/kaizen/skywear/ui/theme/Theme.kt
+++ SkyWear/app/src/main/java/com/kaizen/skywear/ui/theme/Theme.kt
@@ -11,26 +11,30 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
+// dark mode
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    primary = SkyBlue,
+    secondary = SeoulGrey,
+    tertiary = OsakaOrange,
+    background = Color(0xFF121212), //dark
+    surface = Color(0xFF121212),
     onPrimary = Color.White,
     onSecondary = Color.White,
-    onTertiary = Color.White,
+    onTertiary = Color.White
+)
+
+// light mode
+private val LightColorScheme = lightColorScheme(
+    primary = SkyBlue,
+    secondary = SeoulGrey,
+    tertiary = OsakaOrange,
+    background = BackgroundWhite,
+    surface = BackgroundWhite,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
-    */
+
 )
 
 @Composable
