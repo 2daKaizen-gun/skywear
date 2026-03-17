c4cccec278e55ac41c875f3412b91f3bc80af49c
2daKaizen-gun <hkys1223@gmail.com>
2026-03-17 15:01:33 +0900

    feat: define brand color palette(Seoul Blue & Osaka Orange)

--- SkyWear/app/src/main/java/com/kaizen/skywear/ui/theme/Color.kt
+++ SkyWear/app/src/main/java/com/kaizen/skywear/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.kaizen.skywear.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val SkyBlue = Color(0xFF0077B6)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val SeoulGrey = Color(0xFF455A64)
+
+val OsakaOrange = Color(0xFFFF8C42)
+
+val BackgroundWhite = Color(0xFFF8F9FA)
