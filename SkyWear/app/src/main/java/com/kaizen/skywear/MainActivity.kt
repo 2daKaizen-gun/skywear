eb49acfd8fef647d8486d12f6d811af926310be5
2daKaizen-gun <hkys1223@gmail.com>
2026-03-20 00:47:56 +0900

    feat: add ThemePreviewScreen to MainActivity for design system verification 7

--- SkyWear/app/src/main/java/com/kaizen/skywear/MainActivity.kt
+++ SkyWear/app/src/main/java/com/kaizen/skywear/MainActivity.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.kaizen.skywear.ui.theme.SkyWearTheme
+import androidx.compose.ui.unit.dp
+import com.kaizen.skywear.ui.theme.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
