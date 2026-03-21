472229cdf66a9d84dbba356dcc0cf7a757162942
2daKaizen-gun <hkys1223@gmail.com>
2026-03-21 09:47:48 +0900

    feat: add WeatherResponse DTO for OpenWeatherMap API

--- SkyWear/app/src/main/java/com/kaizen/skywear/data/model/WeatherResponse.kt
+++ SkyWear/app/src/main/java/com/kaizen/skywear/data/model/WeatherResponse.kt
@@ -1,4 +1,26 @@
 package com.kaizen.skywear.data.model
 
-class WeatherResponse {
-}
+// OpenWeatherMap API 응답 데이터 클래스
+data class WeatherResponse(
+
+)
+
+data class MainWeather(
+
+)
+
+data class Weather(
+
+)
+
+data class Wind(
+
+)
+
+data class Sys(
+
+)
+
+data class Coord(
+
+)
