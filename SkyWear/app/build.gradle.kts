d74ecc69ff48fcdca10e7406685aa8532de8d6b5
2daKaizen-gun <hkys1223@gmail.com>
2026-03-21 09:21:30 +0900

    feat: architect remote data source with Retrofit2 and OkHttp3

--- SkyWear/app/build.gradle.kts
+++ SkyWear/app/build.gradle.kts
@@ -60,6 +60,10 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging.interceptor)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -67,4 +71,5 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
 }
