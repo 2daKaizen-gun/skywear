76b1e64c5ef277228f5c4afaf4497a7e2f0ed71b
2daKaizen-gun <hkys1223@gmail.com>
2026-03-21 09:51:40 +0900

    feat: add WeatherResponse DTO for OpenWeatherMap API 2

--- SkyWear/app/src/main/java/com/kaizen/skywear/data/model/WeatherResponse.kt
+++ SkyWear/app/src/main/java/com/kaizen/skywear/data/model/WeatherResponse.kt
@@ -1,8 +1,17 @@
 package com.kaizen.skywear.data.model
 
+import com.google.gson.annotations.SerializedName
+import java.sql.Timestamp
+
 // OpenWeatherMap API 응답 데이터 클래스
 data class WeatherResponse(
-
+    @SerializedName("name") val cityName: String, // 도시명(ex: Seoul)
+    @SerializedName("main") val main: MainWeather, // 온/습도 정보
+    @SerializedName("weather") val weather: List<Weather>, //날씨 상태
+    @SerializedName("wind") val wind: Wind, // 바람 정보
+    @SerializedName("sys") val sys: Sys, // 국가 코드
+    @SerializedName("coord") val coord: Coord, // 위/경도
+    @SerializedName("dt") val timestamp: Long // 데이터 수집 시각(Unix)
 )
 
 data class MainWeather(
