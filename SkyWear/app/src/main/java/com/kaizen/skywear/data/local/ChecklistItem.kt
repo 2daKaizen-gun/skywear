17aad4fdddbc1de9961e727147748b03d8fc8f18
2daKaizen-gun <hkys1223@gmail.com>
2026-03-30 00:30:22 +0900

    feat: add Checklist Category in ChecklistItem.kt

--- SkyWear/app/src/main/java/com/kaizen/skywear/data/local/ChecklistItem.kt
+++ SkyWear/app/src/main/java/com/kaizen/skywear/data/local/ChecklistItem.kt
@@ -1,2 +1,19 @@
 package com.kaizen.skywear.data.local
 
+// ChecklistItem
+// Room DB Entity - 일본 여행 체크리스트 아이템
+
+
+data class ChecklistItem(
+
+)
+
+// Checklist Category
+enum class ChecklistCategory {
+    DOCUMENT, // 서류 - 여권, 비자
+    MONEY, // 금융 - 환전, 카드
+    ELECTRONIC, // 전자기기- 어댑터, 충전기
+    CLOTHING, // 의류 - 옷, 신발
+    HEALTH, // 건강 - 상비약, 마스크
+    MISC // 기타
+}
