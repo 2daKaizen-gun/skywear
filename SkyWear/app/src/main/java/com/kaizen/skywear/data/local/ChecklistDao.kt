15dfeccec674fec32e7637172997875bf5aff6f5
2daKaizen-gun <hkys1223@gmail.com>
2026-03-31 09:35:37 +0900

    feat: add Query in interface

--- SkyWear/app/src/main/java/com/kaizen/skywear/data/local/ChecklistDao.kt
+++ SkyWear/app/src/main/java/com/kaizen/skywear/data/local/ChecklistDao.kt
@@ -14,26 +14,40 @@ interface ChecklistDao {
     fun getAllItems(): Flow<List<ChecklistItem>>
 
     // 카테고리별 조회
+    @Query("SELECT * FROM checklist_items ")
+    fun getItemsByCategory(category: ChecklistCategory): Flow<List<ChecklistItem>>
 
     // 체크 안 된 아이템만 조회
     @Query("SELECT * FROM checklist_items WHERE isChecked = 0")
     fun getUncheckedItems(): Flow<List<ChecklistItem>>
 
     // 완료된 아이템 수 조회
+    @Query("SELECT * FROM checklist_items WHERE isChecked = 1")
+    fun getCheckedCount(): Flow<Int>
 
     // 전체 아이템 수 조회
+    @Query("SELECT * FROM checklist_items")
+    fun getTotalCount(): Flow<Int>
 
     // 아이템 추가
 
+
     // 여러 아이템 한 번에 추가 (기본 아이템 초기화에 사용)
+
 
     // 아이템 수정
 
+
     // 체크 상태만 토글
+
 
     // 아이템 삭제
 
+
     // 체크된 아이템 전체 삭제
 
+
     // 전체 초기화 (기본 아이템만 남기기)
+
+
 }
