a927417efdd8aa1c261f47b5609fb2dff46548da
2daKaizen-gun <hkys1223@gmail.com>
2026-04-09 01:44:46 +0900

    feat: add SkyWearApplication for app-level initialization

--- SkyWear/app/src/main/java/com/kaizen/skywear/SkyWearApplication.kt
+++ SkyWear/app/src/main/java/com/kaizen/skywear/SkyWearApplication.kt
@@ -1,11 +1,17 @@
 package com.kaizen.skywear
 
 import android.app.Application
+import com.kaizen.skywear.worker.NotificationScheduler
 
 // 앱 전역 초기화 담당
 // WorkManager 알림 스케줄 등록
 
 class SkyWearApplication : Application() {
 
+    override fun onCreate() {
+        super.onCreate()
 
+        // 앱 시작 시 매일 오전 8시 알림 스케줄 등록
+        NotificationScheduler.scheduleDailyBriefing(this)
+    }
 }
