028875a4981702f5d1097b910d75a4da61db768b
2daKaizen-gun <hkys1223@gmail.com>
2026-04-10 00:33:59 +0900

    feat: make plan for AnimatedWeatherCard.kt

--- SkyWear/app/src/main/java/com/kaizen/skywear/ui/component/AnimatedWeatherCard.kt
+++ SkyWear/app/src/main/java/com/kaizen/skywear/ui/component/AnimatedWeatherCard.kt
@@ -1,2 +1,23 @@
 package com.kaizen.skywear.ui.component
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.kaizen.skywear.data.model.WeatherResponse
+import com.kaizen.skywear.domain.WeatherVisual
+
+// 날씨 카드 - Lottie 애니메이션 + 등장 애니메이션 포함
+// Dual-City Dashboard 메인 카드 컴포넌트
+
+@Composable
+fun AnimatedWeatherCard(
+    weather: WeatherResponse,
+    visual: WeatherVisual,
+    countryColor: androidx.compose.ui.graphics.Color,
+    countryFlag: String,
+    modifier: Modifier = Modifier
+) {
+    // 카드 등장 애니메이션
+
+    // 온도 숫자 스케일 애니메이션
+
+}
