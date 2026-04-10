8bacbfca615bc91ec9ec53de972c649a7cf5121e
2daKaizen-gun <hkys1223@gmail.com>
2026-04-11 00:10:21 +0900

    feat: add lottie weather animation in AnimatedWeatherCard.kt

--- SkyWear/app/src/main/java/com/kaizen/skywear/ui/component/AnimatedWeatherCard.kt
+++ SkyWear/app/src/main/java/com/kaizen/skywear/ui/component/AnimatedWeatherCard.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -28,6 +29,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.kaizen.skywear.data.model.WeatherResponse
+import com.kaizen.skywear.data.model.weatherId
 import com.kaizen.skywear.domain.WeatherVisual
 
 // 날씨 카드 - Lottie 애니메이션 + 등장 애니메이션 포함
@@ -92,13 +94,22 @@ fun AnimatedWeatherCard(
                 Spacer(modifier = Modifier.height(8.dp))
 
                 // Lottie 날씨 애니메이션
+                WeatherAnimation(
+                    weatherId = weather.weatherId(),
+                    isNight = visual.isNight,
+                    modifier = Modifier.size(80.dp)
+                )
 
+                Spacer(modifier = Modifier.height(8.dp))
 
                 // 온도 (스케일 애니메이션)
 
+
                 // 코디 추천
 
+
                 // 습도 + 풍속
+
 
             }
         }
