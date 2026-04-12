de401e52b4229eaf36e254d4c3c44cab0d398ea7
2daKaizen-gun <hkys1223@gmail.com>
2026-04-13 00:01:08 +0900

    test: add Compose UI tests for ThemePreviewScreen with Test Rule

--- SkyWear/app/src/androidTest/java/com/kaizen/skywear/ThemePreviewScreenTest.kt
+++ SkyWear/app/src/androidTest/java/com/kaizen/skywear/ThemePreviewScreenTest.kt
@@ -1,7 +1,22 @@
 package com.kaizen.skywear
 
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+
 // Compose Test Rule 기반 UI 테스트
 
 class ThemePreviewScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    // 기본 UI 요소 표시 여부
+
+
+    // 다크 모드 스위치 동작 검증
+
+
+    // Typography 샘플 표시 여부
+
 
 }
