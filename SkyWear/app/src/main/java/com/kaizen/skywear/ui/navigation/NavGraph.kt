8f5b87ff0ef48d8e7c5056ac9852c578e7bc5d3b
2daKaizen-gun <hkys1223@gmail.com>
2026-04-19 01:25:20 +0900

    feat: add checklist screen import

--- SkyWear/app/src/main/java/com/kaizen/skywear/ui/navigation/NavGraph.kt
+++ SkyWear/app/src/main/java/com/kaizen/skywear/ui/navigation/NavGraph.kt
@@ -5,6 +5,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.kaizen.skywear.ui.screen.ChecklistScreen
 import com.kaizen.skywear.ui.screen.DashboardScreen
 import com.kaizen.skywear.ui.screen.SearchScreen
 
